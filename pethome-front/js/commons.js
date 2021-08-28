a871235f8003743aec3620cb385f8cf802d03d39
shukei <857479185@qq.com>
2021-08-28 08:16:36 +0800

    宠物领养支付(创建订单)

--- pethome-front/js/commons.js
+++ pethome-front/js/commons.js
@@ -1,13 +1,13 @@
 // 1.设置axios全局路径
-axios.defaults.baseURL='http://localhost:8080'
+axios.defaults.baseURL='http://localhost:8080';
 // 2.全局配置axios,给Vue添加要给属性
 Vue.prototype.$http = axios;
 
 // 3.配置前置拦截器
 axios.interceptors.request.use(config => {
     // 获取token
-    let token = localStorage.getItem("token")
-    console.debug(config);
+    let token = localStorage.getItem("token");
+    // console.debug(config);
     if (token) {
         // 如果有token,将token添加到请求头中
         config.headers['TOKEN'] = token;
@@ -15,15 +15,15 @@ axios.interceptors.request.use(config => {
     return config;
 }, error => {
     Promise.reject(error);
-})
+});
 
 // 4.配置后置拦截器
 axios.interceptors.response.use(config => {
-    // console.debug(config);
-    return config;
-    if (!config.data.success && config.data.message === "noAuth") {
+    // console.debug(config.data);
+    if (config.data.message === "noAuth" && !config.data.success) {
         location.href = "/login.html";
     }
+    return config;
 }, error => {
     Promise.reject(error);
-})
+});
